6145c30e659ee3f2a8aecb4ce8963e5d6883f28e
Estarlin R <estarlincito@Estarlincito-2.local>
2023-02-20 20:09:32 -0400

    2.1

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -5,12 +5,19 @@ const HomePage = () => {
   return (
     <>
       <Head>
+        <meta name="title" content="CHATGPT001" />
+        <meta name="description" content="Estarlincito AI" />
         <link
           rel="shortcut icon"
           href="https://raw.githubusercontent.com/estarlincito/chatgpt001/main/src/assets/favicon.ico"
         />
-        <meta name="title" content="CHATGPT001"></meta>
-        <meta name="description" content="Estarlincito AI"></meta>
+        <meta property="og:type" content="website" />
+        <meta property="og:url" content="chatgpt001.vercel.app" />
+
+        <meta
+          property="og:image"
+          content="https://raw.githubusercontent.com/estarlincito/chatgpt001/main/src/assets/favicon.ico"
+        />
         <title>CHATGPT001</title>
       </Head>
 
