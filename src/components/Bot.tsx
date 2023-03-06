0c1d72c06215fcc0d720263cf8befcdb4bb6a311
Estarlin R <estarlincito@Estarlincito-2.local>
2023-03-06 11:19:48 -0400

    2.9

--- src/components/Bot.tsx
+++ src/components/Bot.tsx
@@ -2,7 +2,7 @@ import { botTp } from "../types/contextTp";
 
 const Bot = ({ date, answer }: botTp) => {
   return (
-    <div className="bg-secondary text-primary p-2 r rounded-tr-lg rounded-bl-lg grid max-w-[85%]">
+    <div className="bg-secondary text-primary p-2 r rounded-tr-lg rounded-bl-lg grid max-w-[85%] duration-500">
       <p dangerouslySetInnerHTML={{ __html: answer }} />
       <span className="font-lg text-xs justify-self-end select-none">
         {date}
--- src/components/Human.tsx
+++ src/components/Human.tsx
@@ -2,7 +2,7 @@ import { humanTp } from "../types/contextTp";
 
 const Human = ({ date, question }: humanTp) => {
   return (
-    <div className="bg-quaternary text-primary p-2 r rounded-tl-lg rounded-br-lg justify-self-end grid max-w-[85%]">
+    <div className="bg-quaternary text-primary p-2 r rounded-tl-lg rounded-br-lg justify-self-end grid max-w-[85%] duration-500">
       <p>{question}</p>
       <span className="font-lg text-xs justify-self-end select-none">
         {date}
