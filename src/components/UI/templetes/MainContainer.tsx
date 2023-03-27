9b8d12137dbc9a555ac9d7ab0a02c084947adffd
Estarlin R <estarlincito@Estarlincito-2.local>
2023-03-27 15:24:12 -0400

    4.6.2

--- src/components/UI/templetes/MainContainer.tsx
+++ src/components/UI/templetes/MainContainer.tsx
@@ -6,8 +6,8 @@ const MainContainer = () => {
     <main className="p-2 grid items-center justify-center h-screen">
       <div className="w-96 h-[80vh] duration-500 drop-shadow-lg md:w-[500px]">
         <Nav />
-        {/* <MainChat />
-        <Form /> */}
+        {/* <MainChat /> */}
+        <Form />
       </div>
     </main>
   );
