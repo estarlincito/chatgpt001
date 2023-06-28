0b8bb2bcd19e22b11605dd3b4829a0a954e5099a
Estarlin R <estarlincito@Estarlincito-2.local>
2023-06-28 03:54:39 -0400

    disable link

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,12 @@
-import Link from 'next/link';
+import Chatgpt001 from '@/components/UI/templetes/chatgpt001';
 
 const HomePage = () => {
   return (
     <main className='container grid h-screen mx-auto place-items-center md:p-5'>
-      {/* <Chatgpt001 /> */}
-      <Link href='https://estarlincito.com/gpt'>
+      <Chatgpt001 />
+      {/* <Link href='https://estarlincito.com/gpt'>
         Please go to: estarlincito.com/gpt
-      </Link>
+      </Link> */}
     </main>
   );
 };
