0472ea16d128951b081260d8bc68cc126a63afa6
Estarlin R <estarlincito@Estarlincito-2.local>
2023-09-12 12:39:47 -0400

    add bun support

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,30 +1,27 @@
-import Head from 'next/head';
 import Link from 'next/link';
+
+export const metadata = {
+  title: 'Página no encontrada',
+};
 
 const NotFounPage = () => {
   return (
-    <>
-      <Head>
-        <title>Página no encontrada</title>
-      </Head>
+    <main className='grid justify-center items-center h-[90vh]'>
+      <div>
+        <h3 className='text-3xl'>Lo sentimos…</h3>
+        <p className='text-2xl'>Esta página no existe.</p>
 
-      <main className='grid justify-center items-center h-[90vh]'>
-        <div>
-          <h3 className='text-3xl'>Lo sentimos…</h3>
-          <p className='text-2xl'>Esta página no existe.</p>
-
-          <p>
-            Puedes volver a&nbsp;
-            <Link
-              href='/'
-              className='font-bold text-slate-900 hover:text-slate-600 active:text-slate-500 duration-300'
-            >
-              inicio
-            </Link>
-          </p>
-        </div>
-      </main>
-    </>
+        <p>
+          Puedes volver a&nbsp;
+          <Link
+            href='/'
+            className='font-bold text-slate-900 hover:text-slate-600 active:text-slate-500 duration-300'
+          >
+            inicio
+          </Link>
+        </p>
+      </div>
+    </main>
   );
 };
 
