d53cc5640adaa95301afa37a1cafd3aa87174909
Estarlin R <estarlincito@Estarlincitos-Macbook-Air.local>
2023-10-02 02:55:09 -0400

    ok fixed.....

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,7 @@ const HomePage = () => {
   return (
     <main className='container grid h-screen mx-auto place-items-center md:p-5'>
       <p>
-        This project is closed because now I'm using this:&apos;
+        This project is closed because now I&apos;m using this:{' '}
         <Link
           href='https://estarlincito.com/gpt'
           className='text-black font-bold'
